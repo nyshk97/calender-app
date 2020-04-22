ad336188c13d64e280c2f20f6bccc56e1564b4d8
tsubasa namatame <nyshk97@gmail.com>
2020-04-23 08:56:29 +0900

    First commit

---
+++ redux-sample/src/components/Counter/container.jsx
@@ -0,0 +1,15 @@
+import { connect } from "react-redux";
+import { increment, decrement } from "../../redux/count/actions";
+import Counter from "./presentation";
+
+const mapStateProps = ({ count }) => ({ count });
+const mapDispatchProps = (dispatch) => ({
+  increment: (count) => {
+    dispatch(increment(count));
+  },
+  decrement: (count) => {
+    dispatch(decrement(count));
+  },
+});
+
+export default connect(mapStateProps, mapDispatchProps)(Counter);
---
+++ redux-sample/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+import rootReducer from "./redux/rootReducer";
+
+const store = createStore(rootReducer);
+
+import Counter from "./components/Counter/container";
+
+const App = () => (
+  <Provider store={store}>
+    <Counter />
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
