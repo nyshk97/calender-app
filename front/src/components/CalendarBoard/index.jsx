6cb3656bf348be19472be74b970c94605811f5e9
tsubasa namatame <nyshk97@gmail.com>
2020-04-23 19:17:40 +0900

    Complete Capter2

--- front/src/components/CalendarBoard/index.jsx
+++ front/src/components/CalendarBoard/index.jsx
@@ -1,52 +1,34 @@
 import React from "react";
-import { GridList } from "@material-ui/core";
+import { GridList, Typography } from "@material-ui/core";
 import * as styles from "./style.css";
+import CalenderElement from "../CalendarElement";
+import { createCalendar } from "../../services/calendar";
 
-const calendar = [
-  "29",
-  "30",
-  "10月1日",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "13",
-  "14",
-  "15",
-  "16",
-  "17",
-  "18",
-  "19",
-  "20",
-  "21",
-  "22",
-  "23",
-  "24",
-  "25",
-  "26",
-  "27",
-  "28",
-  "29",
-  "30",
-  "31",
-  "11月1日",
-  "2",
-];
+const calendar = createCalendar();
+
+const days = ["日", "月", "火", "水", "木", "金", "土"];
 
 const CalendarBoard = () => {
+  console.log(calendar);
   return (
     <div className={styles.container}>
       <GridList className={styles.grid} cols={7} spacing={0} cellHeight="auto">
+        {days.map((d) => (
+          <li key={d}>
+            <Typography
+              className={styles.days}
+              color="textSecondary"
+              align="center"
+              variant="caption"
+              component="div"
+            >
+              {d}
+            </Typography>
+          </li>
+        ))}
         {calendar.map((c) => (
-          <li>
-            <div className={styles.element}>{c}</div>
+          <li key={c.toISOString()}>
+            <CalenderElement day={c} />
           </li>
         ))}
       </GridList>
